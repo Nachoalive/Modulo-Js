d887d5d1b95698f566ac91339d65c40ff05ffa78
Ignacio Diaz <ignacio.adz@gmail.com>
2024-07-29 09:27:54 -0400

    primera entrega

---
+++ js/index.js
@@ -0,0 +1,33 @@
+
+const IVA = 1.19;
+let valorPvp;
+let valorCosto;
+
+
+do {
+    valorPvp = parseFloat(prompt("Ingrese el valor de Venta"));
+    if (valorPvp <= 0 || isNaN(valorPvp)) {
+        alert("El dato ingresado no es válido para el valor de venta. Intente de nuevo.");
+    }
+} while (valorPvp <= 0 || isNaN(valorPvp));
+
+do {
+    valorCosto = parseFloat(prompt("Ingrese el valor costo"));
+    if (valorCosto <= 0 || isNaN(valorCosto)) {
+        alert("El dato ingresado no es válido para el costo. Intente de nuevo.");
+    }
+} while (valorCosto <= 0 || isNaN(valorCosto));
+
+function dividir(n1) {
+    return n1 / IVA;
+}
+
+const valorNeto = dividir(valorPvp);
+
+const utilidad = valorNeto - valorCosto;
+
+const margen = (utilidad / valorNeto) * 100;
+
+alert("La utilidad es: " + utilidad);
+
+alert("El porecntaje de utilidad es de: " + margen);
